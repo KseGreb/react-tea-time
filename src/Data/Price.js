8b7d55dba45132370cc7fcb8768664a912297426
Ksenia <110953599+KseGreb@users.noreply.github.com>
2024-04-27 11:06:15 -0600

    created data price

---
+++ src/Data/Price.js
@@ -0,0 +1,107 @@
+const Price = [
+    {
+        id: 1,
+        teaName: "Green tea",
+        image: "tea1",
+        price: 5,
+        category: "GREEN"
+    },
+    {
+        id: 2,
+        teaName: "Green tea",
+        image: "tea1",
+        price: 5,
+        category: "GREEN"
+    },
+    {
+        id: 3,
+        teaName: "Green tea",
+        image: "tea1",
+        price: 5,
+        category: "GREEN"
+    },
+    {
+        id: 4,
+        teaName: "Green tea",
+        image: "tea1",
+        price: 5,
+        category: "GREEN"
+    },
+    {
+        id: 5,
+        teaName: "Green tea",
+        image: "tea1",
+        price: 5,
+        category: "GREEN"
+    },
+    {
+        id: 6,
+        teaName: "Green tea",
+        image: "tea1",
+        price: 5,
+        category: "GREEN"
+    },
+    {
+        id: 7,
+        teaName: "Green tea",
+        image: "tea1",
+        price: 5,
+        category: "GREEN"
+    },
+    {
+        id: 8,
+        teaName: "Green tea",
+        image: "tea1",
+        price: 5,
+        category: "GREEN"
+    },
+    {
+        id: 9,
+        teaName: "Green tea",
+        image: "tea1",
+        price: 5,
+        category: "GREEN"
+    },
+    {
+        id: 10,
+        teaName: "Green tea",
+        image: "tea1",
+        price: 5,
+        category: "GREEN"
+    },
+    {
+        id: 11,
+        teaName: "Green tea",
+        image: "tea1",
+        price: 5,
+        category: "GREEN"
+    },
+    {
+        id: 12,
+        teaName: "Green tea",
+        image: "tea1",
+        price: 5,
+        category: "GREEN"
+    },
+    {
+        id: 13,
+        teaName: "Green tea",
+        image: "tea1",
+        price: 5,
+        category: "GREEN"
+    },
+    {
+        id: 14,
+        teaName: "Green tea",
+        image: "tea1",
+        price: 5,
+        category: "GREEN"
+    },
+    {
+        id: 15,
+        teaName: "Green tea",
+        image: "tea1",
+        price: 5,
+        category: "GREEN"
+    },
+]
