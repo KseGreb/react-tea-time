f88683029cc21622abf1c458f3ae4b1aa7cabd98
Ksenia <110953599+KseGreb@users.noreply.github.com>
2024-04-28 11:44:43 -0600

    working with price data

--- src/Data/Price.js
+++ src/Data/Price.js
@@ -1,28 +1,28 @@
 const Price = [
     {
         id: 1,
-        teaName: "Green tea",
+        teaName: "Green tea (Sri Lanka)",
         image: "tea1",
         price: 5,
         category: "GREEN"
     },
     {
         id: 2,
-        teaName: "White tea",
+        teaName: "White tea (Tanzania)",
         image: "tea2",
         price: 7,
         category: "WHITE"
     },
     {
         id: 3,
-        teaName: "Black tea",
+        teaName: "Black tea (Kenya)",
         image: "tea3",
         price: 4,
         category: "BLACK"
     },
     {
         id: 4,
-        teaName: "Oolong tea",
+        teaName: "Oolong tea (Japan)",
         image: "tea1",
         price: 6,
         category: "OOLONG"
@@ -36,28 +36,28 @@ const Price = [
     },
     {
         id: 6,
-        teaName: "Green tea",
+        teaName: "Green tea (Turkey)",
         image: "tea1",
         price: 5,
         category: "GREEN"
     },
     {
         id: 7,
-        teaName: "White tea",
+        teaName: "White tea (Vietnam)",
         image: "tea2",
         price: 5,
         category: "WHITE"
     },
     {
         id: 8,
-        teaName: "Black tea",
+        teaName: "Black tea (India)",
         image: "tea3",
         price: 5,
         category: "BLACK"
     },
     {
         id: 9,
-        teaName: "Oolong tea",
+        teaName: "Oolong tea (China)",
         image: "tea1",
         price: 5,
         category: "OOLONG"
@@ -71,7 +71,7 @@ const Price = [
     },
     {
         id: 11,
-        teaName: "Green tea",
+        teaName: "Green tea (Japan)",
         image: "tea1",
         price: 5,
         category: "GREEN"
@@ -85,28 +85,28 @@ const Price = [
     },
     {
         id: 13,
-        teaName: "Black tea",
+        teaName: "Black tea (Sri Lanka)",
         image: "tea3",
         price: 5,
         category: "BLACK"
     },
     {
         id: 14,
-        teaName: "Oolong tea",
+        teaName: "Oolong tea (India)",
         image: "tea1",
         price: 5,
         category: "OOLONG"
     },
     {
         id: 15,
-        teaName: "Herbal tea",
+        teaName: "Herbal tea (mix, Vietnam)",
         image: "tea4",
         price: 5,
         category: "HERBAL"
     },
     {
         id: 16,
-        teaName: "Green tea",
+        teaName: "Green tea (Kenya)",
         image: "tea1",
         price: 5,
         category: "GREEN"
@@ -120,14 +120,14 @@ const Price = [
     },
     {
         id: 18,
-        teaName: "Black tea",
+        teaName: "Black tea (Tanzania)",
         image: "tea3",
         price: 5,
         category: "BLACK"
     },
     {
         id: 19,
-        teaName: "Oolong tea",
+        teaName: "Oolong tea (Indonesia)",
         image: "tea1",
         price: 5,
         category: "OOLONG"
