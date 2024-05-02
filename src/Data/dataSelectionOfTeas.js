7e65d52fe1cd757526b842ea18ac73d9f1070162
Ksenia <110953599+KseGreb@users.noreply.github.com>
2024-05-02 14:53:46 -0600

    adding pictures of tea for online store

--- src/Data/dataSelectionOfTeas.js
+++ src/Data/dataSelectionOfTeas.js
@@ -2,140 +2,140 @@ export const dataSecectionOfTeas = [
     {
         id: 1,
         teaName: "Green tea (Sri Lanka)",
-        image: "tea1",
+        image: "green",
         price: 5,
         category: "GREEN"
     },
     {
         id: 2,
         teaName: "White tea (Tanzania)",
-        image: "tea2",
+        image: "white",
         price: 7,
         category: "WHITE"
     },
     {
         id: 3,
         teaName: "Black tea (Kenya)",
-        image: "tea3",
+        image: "black",
         price: 4,
         category: "BLACK"
     },
     {
         id: 4,
         teaName: "Oolong tea (Japan)",
-        image: "tea1",
+        image: "green",
         price: 6,
         category: "OOLONG"
     },
     {
         id: 5,
         teaName: "Peppermint tea",
-        image: "tea4",
+        image: "herbal",
         price: 3,
         category: "HERBAL"
     },
     {
         id: 6,
         teaName: "Green tea (Turkey)",
-        image: "tea1",
+        image: "green",
         price: 4.5,
         category: "GREEN"
     },
     {
         id: 7,
         teaName: "White tea (Vietnam)",
-        image: "tea2",
+        image: "white",
         price: 6,
         category: "WHITE"
     },
     {
         id: 8,
         teaName: "Black tea (India)",
-        image: "tea3",
+        image: "black",
         price: 4,
         category: "BLACK"
     },
     {
         id: 9,
         teaName: "Oolong tea (China)",
-        image: "tea1",
+        image: "green",
         price: 5,
         category: "OOLONG"
     },
     {
         id: 10,
         teaName: "Chamomile tea",
-        image: "tea4",
+        image: "herbal",
         price: 5,
         category: "HERBAL"
     },
     {
         id: 11,
         teaName: "Green tea (Japan)",
-        image: "tea1",
+        image: "green",
         price: 8,
         category: "GREEN"
     },
     {
         id: 12,
         teaName: "White tea",
-        image: "tea2",
+        image: "white",
         price: 4,
         category: "WHITE"
     },
     {
         id: 13,
         teaName: "Black tea (Sri Lanka)",
-        image: "tea3",
+        image: "black",
         price: 6,
         category: "BLACK"
     },
     {
         id: 14,
         teaName: "Oolong tea (India)",
-        image: "tea1",
+        image: "green",
         price: 8,
         category: "OOLONG"
     },
     {
         id: 15,
         teaName: "Herbal tea (mix, Vietnam)",
-        image: "tea4",
+        image: "herbal",
         price: 6,
         category: "HERBAL"
     },
     {
         id: 16,
         teaName: "Green tea (Kenya)",
-        image: "tea1",
+        image: "green",
         price: 5.5,
         category: "GREEN"
     },
     {
         id: 17,
         teaName: "White tea",
-        image: "tea2",
+        image: "white",
         price: 5,
         category: "WHITE"
     },
     {
         id: 18,
         teaName: "Black tea (Tanzania)",
-        image: "tea3",
+        image: "black",
         price: 7,
         category: "BLACK"
     },
     {
         id: 19,
         teaName: "Oolong tea (Indonesia)",
-        image: "tea1",
+        image: "green",
         price: 6,
         category: "OOLONG"
     },
     {
         id: 20,
         teaName: "Herbal tea",
-        image: "tea4",
+        image: "herbal",
         price: 3.5,
         category: "HERBAL"
     },
--- src/Pages/OnlineStore/TeaComponents/TeaSelection.js
+++ src/Pages/OnlineStore/TeaComponents/TeaSelection.js
@@ -2,5 +2,6 @@ export const TeaSelection = ({tea}) => {
     return(<div>
         <h2>{tea.teaName}</h2>
         <p>${tea.price}</p>
+        <img src={`./${tea.img}.jpg`} alt="tea"/>
     </div>)
 }
