6607156a37ec64842c903fecd1401787bef864e7
Ksenia <110953599+KseGreb@users.noreply.github.com>
2024-05-02 14:05:41 -0600

    working on online store

--- src/Pages/OnlineStore/OnlineStore.js
+++ src/Pages/OnlineStore/OnlineStore.js
@@ -1,16 +1,22 @@
-import { AllCategories } from "./AllCategories"
-import { Cart } from "./Cart"
-import { SelectionOfTeas } from "./SelectionOfTeas"
+import { AllCategories } from "./Filter/AllCategories"
+import { Cart } from "./Cart/Cart"
+import { SelectionOfTeas } from "./TeaComponents/SelectionOfTeas"
 
 export const OnlineStore = () => {
     console.log(OnlineStore)
     return(<div>
         
-        OnlineStore
-    
-        <AllCategories/>
+        <div className="mainSelection">
+            <div className="selection">
+                <AllCategories/>
+            </div>
+            <div className="selection">
+                <SelectionOfTeas/>
+            </div>
+
+        </div>
         <Cart/>
-        <SelectionOfTeas/>
+        
         
     </div>)
 }
