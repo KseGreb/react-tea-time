ca07cf3008ba38c217a076fa1813fe98e2ce4f2f
Ksenia <110953599+KseGreb@users.noreply.github.com>
2024-05-03 11:37:57 -0600

    added images to the teas

--- src/Pages/OnlineStore/TeaComponents/TeaSelection.js
+++ src/Pages/OnlineStore/TeaComponents/TeaSelection.js
@@ -2,6 +2,6 @@ export const TeaSelection = ({tea}) => {
     return(<div>
         <h2>{tea.teaName}</h2>
         <p>${tea.price}</p>
-        <img src={`./${tea.img}.jpg`} alt="tea"/>
+        <img src={`./${tea.image}.jpg`} alt="tea" width="350px" height="420px"/>
     </div>)
 }
