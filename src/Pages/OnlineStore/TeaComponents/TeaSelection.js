a26d8e504ec18c621126a1afb7f3df2306f16a6f
Ksenia <110953599+KseGreb@users.noreply.github.com>
2024-05-05 11:03:09 -0600

    working with tea selection for online store

--- src/Pages/OnlineStore/TeaComponents/TeaSelection.js
+++ src/Pages/OnlineStore/TeaComponents/TeaSelection.js
@@ -1,9 +1,9 @@
 export const TeaSelection = ({tea}) => {
     return(<div className="teaContainer">
         <div className="allTeaForSale">
+            <img src={`./${tea.image}.jpg`} alt="tea" width="350px" height="420px"/>
             <h2>{tea.teaName}</h2>
             <p>${tea.price}</p>
-            <img src={`./${tea.image}.jpg`} alt="tea" width="350px" height="420px"/>
         </div>
     </div>)
 }
